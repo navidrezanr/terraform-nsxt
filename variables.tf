f9ab3169b724c9f3438e4894639e32857d52f3b6
navidrezanr <103016844+navidrezanr@users.noreply.github.com>
2023-08-19 15:01:23 +0330

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,14 @@
+# Variables
+# NSX Manager IP
+variable "nsx_manager" {
+  default = "IP address of your nsx-t manager"
+}
+
+# Username & Password for NSX-T Manager
+variable "username" {
+  default = "user name with privilege"
+}
+
+variable "password" {
+  default = "password of your username"
+}
